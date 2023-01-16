b38c44ea8e98f0c3806e00196a96d457bc2e5b07
Hubert Niewiarowski <73963567+HubertNiew@users.noreply.github.com>
2023-01-16 20:02:01 +0100

    Skrypt tworzący bazę i wczytujący dane z csv

---
+++ db.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS miejsce;
+DROP TABLE IF EXISTS przejazd;
+DROP TABLE IF EXISTS stacja;
+
+CREATE TABLE stacja (
+    nazwa VARCHAR(128),
+    id_trasy INTEGER,
+    nr INTEGER,
+    czas_dojazdu INTEGER
+);
+
+CREATE TABLE przejazd (
+    id INTEGER PRIMARY KEY,
+    id_trasy INTEGER,
+    id_pociagu INTEGER,
+    czas_odjazdu TIMESTAMP
+);
+
+CREATE TABLE miejsce (
+    id INTEGER PRIMARY KEY,
+    id_przejazdu INTEGER REFERENCES przejazd (id),
+    wolne_od INTEGER,
+    wolne_do INTEGER
+);
+
+\COPY stacja FROM 'stacja.csv' (FORMAT csv, HEADER)
+\COPY przejazd FROM 'przejazd.csv' (FORMAT csv, HEADER)
+\COPY miejsce FROM 'miejsce.csv' (FORMAT csv, HEADER)
